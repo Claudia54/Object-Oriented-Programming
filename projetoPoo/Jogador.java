f578afe4b1be74874b78b61d460c3f50fcf6af9f
Gustominox <agstcmps.111@gmail.com>
2021-03-28 10:00:15 +0100

    create Jogador.java part2

---
+++ projetoPoo/Jogador.java
@@ -0,0 +1,16 @@
+public class Jogador{
+    private double velocidade;
+    private double destreza;
+    private double resistencia;
+    private double impulsao;
+    private double jogoDeCabeca;
+    private double remate;
+    private double capDePasse;
+    
+    
+    public double getVelocidade() {
+        return velocidade;
+    }
+    
+
+}
